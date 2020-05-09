ca406ff5af064788b2039b674e59c025e4606857
Nguyen Quoc Huy <huynq8@vng.com.vn>
2020-05-09 16:41:57 +0700

    t

--- DJANGO_NGUYENQUOCHUY/settings.py
+++ DJANGO_NGUYENQUOCHUY/settings.py
@@ -11,13 +11,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR=os.path.join(BASE_DIR,"templates")
 #STATIC_DIR=os.path.join(BASE_DIR,"static")
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -122,5 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
 
 CART_SESSION_ID = 'first_app'
